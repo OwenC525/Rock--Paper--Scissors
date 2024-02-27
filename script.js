3cac9dcee9d99e7d9b94fab0989e510fc81d96ee
OwenC525 <525ochiu@frhsd.com>
2024-02-27 09:28:53 -0500

    worked on js; added name prompt and score change and changed variables

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+let username = prompt('What is your name?'); // COLLECTS PLAYER NAME
 
-let roundsplayed = 0
-let playerscore = 0
-let computerscore = 0
+if (username === null) {
+  username = prompt('Enter your name!'); // ASKS FOR NAME UNTIL VALID RESPONSE IS ENTERED
+}
+
+document.getElementById('username').innerHTML = username; // USES HTML ID TO INSERT USERNAME
+
+// INITIAL SCORING BOARD
+let roundsPlayed = 0
+let playerScore = 0
+let computerScore = 0
 
 
-  const getComputerChoice = () => {
+// COMPUTER CHOICE
+  function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
       case 0:
@@ -16,23 +25,31 @@ let computerscore = 0
     }
   };
   
+
+  // PLAYER CHOICE
   function determineWinner(userChoice) {
+    const computerChoice = getComputerChoice(); // Store the computer's choice
     
-    if (userChoice === getComputerChoice()) {
+    if (userChoice === computerChoice) {
       result.textContent = 'It\'s a tie!';
-      roundsplayed++;
+      roundsPlayed++;
     } 
-    else if (userChoice === 'rock' && getComputerChoice() === 'scissors' || userChoice === 'paper' && getComputerChoice() === 'rock' || userChoice === 'scissors' && getComputerChoice() === 'paper'){
+    else if (userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'scissors' && computerChoice === 'paper'){
       result.textContent = 'You won!';
-      roundsplayed++;
-      playerscore++;
+      roundsPlayed++;
+      playerScore++;
     }  
     else {
       result.textContent = 'You lose!';
-      roundsplayed++;
-      computerscore++;
+      roundsPlayed++;
+      computerScore++;
     }
       
+    document.getElementById('playerScore').innerText = `${username} score: ${playerScore}`
+    document.getElementById('computerScore').innerHTML = `Computer Score: ${computerScore}`
+
+
+
     };
 
     // PUT BUTTON JS HERE LATER??
